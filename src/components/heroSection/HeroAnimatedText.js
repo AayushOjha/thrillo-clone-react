ea8809d3105f24cbf96cfc049874c9b8e77b8dfd
Aayushojha <18egjcs024@gitjaipur.com>
2022-02-23 15:35:29 +0530

    hosted on github pages

--- src/components/heroSection/HeroAnimatedText.js
+++ src/components/heroSection/HeroAnimatedText.js
@@ -1,14 +1,10 @@
-import classes from './HeroAnimatedText.module.css';
+import classes from "./HeroAnimatedText.module.css";
 
 export default function HeroAnimatedText() {
   return (
-    <div className={classes.cointainer} >
-      <h1 className={classes.upperText} >
-      It's time for new
-      </h1>
-      <h1 className={classes.lowerText} >
-      Experiences
-      </h1>
+    <div className={classes.cointainer}>
+      <h1 className={classes.upperText}>It's time for Best</h1>
+      <h1 className={classes.lowerText}>Experiences</h1>
     </div>
-  )
+  );
 }
